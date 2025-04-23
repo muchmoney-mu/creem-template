4f39831cc04ce9f00d651885d78ecc34eef14e17
Gabriel Ferraz <sudoferraz@gmail.com>
2025-04-23 14:32:51 +0300

    Adds creem sdk

--- nextjs-prisma-template/app/api/checkout/route.ts
+++ nextjs-prisma-template/app/api/checkout/route.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
+import { Creem } from "creem";
 
 export interface CheckoutSession {
   id: string;
@@ -12,22 +13,50 @@ export interface CheckoutSession {
 }
 
 export async function GET() {
+  console.log("HERER");
+  const creem = new Creem({
+    serverIdx: 1,
+  });
   const apiKey = process.env.CREEM_API_KEY;
   const productId = process.env.CREEM_PRODUCT_ID;
-  const creemTestUrl = "https://test-api.creem.io/v1/checkouts";
-  const checkoutSessionResponse = await axios.post(
-    creemTestUrl,
-    {
-      product_id: productId,
+  // const creemTestUrl = "https://test-api.creem.io/v1/checkouts";
+  //
+  console.log("calling product");
+  const resultProduct = await creem.createCheckout({
+    xApiKey: apiKey!,
+    createCheckoutRequestEntity: {
+      productId: productId!,
     },
-    { headers: { "x-api-key": apiKey } },
-  );
-  if (checkoutSessionResponse.status !== 200) {
-    return;
-  }
+  });
+  console.log(resultProduct);
+  // const creemTestUrl = "http://localhost:8000/v1/checkouts";
+  // const result = await creem.createCheckout({
+  //   xApiKey: apiKey!,
+  //   createCheckoutRequestEntity: {
+  //     productId: productId!,
+  //   },
+  // });
+  //
+  // console.log(result);
 
+  // const checkoutSessionResponse = await axios.post(
+  //   creemTestUrl,
+  //   {
+  //     product_id: productId,
+  //     units: 4,
+  //   },
+  //   { headers: { "x-api-key": apiKey } },
+  // );
+  // if (checkoutSessionResponse.status !== 200) {
+  //   return;
+  // }
+
+  // return NextResponse.json({
+  //   success: true,
+  //   checkout: checkoutSessionResponse.data as CheckoutSession,
+  // });
   return NextResponse.json({
     success: true,
-    checkout: checkoutSessionResponse.data as CheckoutSession,
+    checkout: true,
   });
 }
--- nextjs-prisma-template/app/layout.tsx
+++ nextjs-prisma-template/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +11,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="salesnip">
+        {`!function(e,n){if(!n.loaded){var t,a,r=n||{};for(r.__queue=[],(n=e.createElement("script")).type="text/javascript",n.async=!0,n.src="https://cdn.salesnip.com/v1/script.min.js",(o=e.getElementsByTagName("script")[0])?o.parentNode.insertBefore(n,o):e.head.appendChild(n),r.__handler=function(e){return function(){r.__queue.push([e].concat(Array.prototype.slice.call(arguments,0)))}},t="open on off".split(" "),a=0;a<t.length;a++){var i=t[a];r[i]=r.__handler(i)}var o=new Proxy(r,{get:function(e,n){return n in e||(e[n]=r.__handler(n)),e[n]}});window.salesnip=o,window.salesnip.loaded=1}}(document,window.salesnip||{});`}
+      </Script>
+
       <body className={"bg-black dark h-full" + inter.className}>
         {children}
       </body>
